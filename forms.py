d8311eb20f130f718162afbd43bcffe6970c0ef8
betadayz <betadayz888@gmail.com>
2020-09-01 12:22:17 +0100

    add owner included

--- forms.py
+++ forms.py
@@ -9,3 +9,8 @@ class AddForm(FlaskForm):
 class DelForm(FlaskForm):
     id = IntegerField('Id Number of Puppy to Remove: ')
     submit = SubmitField('Remove Puppy')    
+    
+class AddOwnerForm(FlaskForm):
+    name = StringField('Name of Owner: ')
+    pup_id = IntegerField('Id of Puppy:')
+    submit = SubmitField('Add Owner')    
